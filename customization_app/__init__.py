8e6506d7bb815b145234a7f185e5267a439c2bdf
Wassim Koubaa <koubaawassim@gmail.com>
2025-11-09 15:40:28 +0000

    improving the monkey patch

--- customization_app/__init__.py
+++ customization_app/__init__.py
@@ -1,4 +1,8 @@
-import erpnext.stock.get_item_details
-import customization_app.get_item_details
-erpnext.stock.get_item_details.get_item_details = customization_app.get_item_details.get_item_details
+try:
+    import erpnext.stock.get_item_details
+    import customization_app.get_item_details
+    erpnext.stock.get_item_details.get_item_details = customization_app.get_item_details.get_item_details
+except ImportError as e:
+    print(f"Monkey patch failed: {e}")
+
 __version__ = "develop"
