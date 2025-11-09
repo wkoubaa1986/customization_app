c435e6ab03e585978fe0661cbc0627d60b7534ea
Wassim Koubaa <koubaawassim@gmail.com>
2025-11-09 15:45:35 +0000

    taking out the patch

--- customization_app/__init__.py
+++ customization_app/__init__.py
@@ -1,8 +1,8 @@
-try:
-    import erpnext.stock.get_item_details
-    import customization_app.get_item_details
-    erpnext.stock.get_item_details.get_item_details = customization_app.get_item_details.get_item_details
-except ImportError as e:
-    print(f"Monkey patch failed: {e}")
+# try:
+#     import erpnext.stock.get_item_details
+#     import customization_app.get_item_details
+#     erpnext.stock.get_item_details.get_item_details = customization_app.get_item_details.get_item_details
+# except ImportError as e:
+#     print(f"Monkey patch failed: {e}")
 
 __version__ = "develop"
